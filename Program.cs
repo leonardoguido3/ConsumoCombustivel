076d007e158c4fe034bd1b1f2d9acfd2e1ce2567
leonardoguido3 <leonardoguido3@gmail.com>
2023-01-06 08:36:37 -0300

    Inicio do Projeto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsumoCombustivel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
